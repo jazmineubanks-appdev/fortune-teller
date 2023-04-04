851afff2b11cfb5b35e73e7dd688bc2b315800d5
Jazmin Eubanks <jazmin.eubanks@gmail.com>
2023-04-04 21:17:11 +0000

    final

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -7,7 +7,18 @@ class NumbersController < ApplicationController
       
       @zebra.push(giraffe)
     end
-
     render({ :template => "lottery_stuff/woohoo.html.erb"})
+
   end 
+  def losers
+    @zebra = Array.new
+
+    5.times do
+      giraffe = rand(1...100)
+      
+      @zebra.push(giraffe)
+    end
+    
+    render({ :template => "lottery_stuff/woowoo.html.erb"})
+  end
 end
